12fb27b10710f1291d2b53ba18cb87991f297d1d
kirtti-nath <kirttinath.ojha@squbix.com>
2024-09-26 15:22:46 +0530

    New Practice examples are added

--- firstclassfunc.js
+++ firstclassfunc.js
@@ -1,13 +1,13 @@
-// First Class Function :
-// A function is a value that can be assigned to a variable and passed as an argument to another function.
-// It's also a type of object in JavaScript, with properties and methods just like any other object.
+//* First Class Function :
+//* A function is a value that can be assigned to a variable and passed as an argument to another function.
+//* It's also a type of object in JavaScript, with properties and methods just like any other object.
 
 var a = function firstcls() {
   console.log("hii");
 };
 a();
 
-//firstcls(function () {});
-// or
+//?firstcls(function () {});
+//? or
 
-//setTimeout(function () {}, 3000);
+//?setTimeout(function () {}, 3000);
--- practice.js
+++ practice.js
@@ -1,8 +1,20 @@
-const inter = function () {
-  console.log("Hello");
-};
+// const arr1 = [1, 2, 3];
+// const arr2 = arr1;
+// console.log(arr2);
+// arr1.pop(1);
+// console.log(arr2);
+// console.log(arr1);
 
-setTimeout(inter, 1000);
+// var x = 2;
+// var y = x;
+// console.log(x);
+// x = 3;
+// console.log(y);
 
-console.log(x);
-var x = 12;
+const fib = n => {
+    if (n <= 1) return 1
+    return fib(n - 1) + fib(n - 2)
+  }
+
+  const res = fib(9);
+  console.log("Result - ",res)
